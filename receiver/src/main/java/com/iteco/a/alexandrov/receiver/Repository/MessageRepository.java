bec478965c97a1d678c0fcf72f55cacfde931051
ai.aleksandrov <vox613@gmail.com>
2020-01-13 15:03:21 +0300

    Missed @Repository annotation added

--- receiver/src/main/java/com/iteco/a/alexandrov/receiver/Repository/MessageRepository.java
+++ receiver/src/main/java/com/iteco/a/alexandrov/receiver/Repository/MessageRepository.java
@@ -2,12 +2,14 @@ package com.iteco.a.alexandrov.receiver.Repository;
 
 import com.iteco.a.alexandrov.receiver.Entity.MessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * Repository implements the functionality of the JPA repository. Provides interoperability with H2 database.
  */
+@Repository
 public interface MessageRepository extends JpaRepository<MessageEntity, Integer> {
     Optional<MessageEntity> getById(long id);
 }
